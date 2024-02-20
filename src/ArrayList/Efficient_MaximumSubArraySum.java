58742666bf4443b4691c2def58b6dbea6e493624
Harshit <hphpal@gmail.com>
2024-02-21 02:39:05 +0530

    Matrix Some Concepts

---
+++ src/ArrayList/Efficient_MaximumSubArraySum.java
@@ -0,0 +1,21 @@
+package arrayList;
+
+public class Efficient_MaximumSubArraySum {
+    public static int maxSum(int[] arr) {
+        int n = arr.length;
+        int maxEnding = arr[0];
+        int res = arr[0];
+        for (int i = 0; i < n; i++) {
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+            res = Math.max(res, maxEnding);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 3};
+
+        int res = maxSum(arr);
+        System.out.println(res);
+    }
+}
